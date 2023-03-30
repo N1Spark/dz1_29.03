90a072589d646a8f35f02ec05712a7e575ed2876
Sarp_dg92@student.itstep.org <Sarp_dg92@student.itstep.org>
2023-03-30 14:47:16 +0300

    Add project files.

---
+++ dz1_29.03/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace dz1_29._03
+{
+    public interface IHandler
+    {
+        IHandler SetNext(IHandler handler);
+        void Handle(Receiver reciever);
+    }
+    public abstract class PaymentHandler : IHandler
+    {
+        public IHandler Handler;
+        public IHandler SetNext(IHandler handler)
+        {
+            Handler = handler;
+            return handler;
+        }
+
+        public virtual void Handle(Receiver reciever)
+        {
+            if (Handler != null)
+                Handler.Handle(reciever);
+        }
+    }
+    public class MoneyPaymentHandler : PaymentHandler
+    {
+        public override void Handle(Receiver reciever)
+        {
+            if (reciever.MoneyTransfer)
+            {
+                Console.WriteLine("Transfer through money transfer systems\n");
+                Handler.Handle(reciever);
+            }
+            else
+                Handler.Handle(reciever);
+        }
+    }
+    public class PayPalPaymentHandler : PaymentHandler
+    {
+        public override void Handle(Receiver reciever)
+        {
+            if (reciever.PayPalTransfer)
+            {
+                Console.WriteLine("Transfer via paypal\n");
+                Handler.Handle(reciever);
+            }
+            else
+                Handler.Handle(reciever);
+        }
+    }
+    public class BankPaymentHandler : PaymentHandler
+    {
+        public override void Handle(Receiver reciever)
+        {
+            if (reciever.BankTransfer)
+                Console.WriteLine("Bank transfer\n");
+            else
+                Handler.Handle(reciever);
+        }
+    }
+    public class Receiver
+    {
+        public bool BankTransfer { get; set; }
+        public bool MoneyTransfer { get; set; }
+        public bool PayPalTransfer { get; set; }
+        public Receiver(bool bt, bool mt, bool ppt)
+        {
+            BankTransfer = bt;
+            MoneyTransfer = mt;
+            PayPalTransfer = ppt;
+        }
+        public void Request(PaymentHandler h, Receiver reciever)
+        {
+            h.Handle(reciever);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var money = new MoneyPaymentHandler();
+            var pal = new PayPalPaymentHandler();
+            var bank = new BankPaymentHandler();
+            money.SetNext(pal).SetNext(bank);
+            Receiver receiver = new Receiver(true, false, true);
+            receiver.Request(pal, receiver);
+        }
+    }
+}
